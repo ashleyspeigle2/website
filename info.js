5f840fd12ee4a13351fe804ae6b5015b0b1b9cd6
Ashley Speigle <ans2@graceland.edu>
2018-04-20 12:47:32 -0500

    website update

---
+++ info.js
@@ -0,0 +1,6 @@
+var name = "Ashley";
+
+document.write("<span id ='footer'>Ashley's Website</span><br />");
+document.write("<span id ='footer'>&#169 2018 Ashley</span><br />");
+document.write("<span id ='footer'>Modified Feb 26 2018</span><br />");
+document.write("<span id ='footer'>Retrieved: Fri Mar 02 2018 13:19:54 GMT-0600(Central Standard Time)</span><br />");
